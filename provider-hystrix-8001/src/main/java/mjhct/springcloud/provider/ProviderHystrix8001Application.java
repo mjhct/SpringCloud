7d946da59ea17d825c17b624824184205e89800e
mjhct <mjhct0358@163.com>
2020-08-16 15:29:23 +0800

    Hystrix服务降级熔断限流:服务提供者侧服务超时降级演示。

--- provider-hystrix-8001/src/main/java/mjhct/springcloud/provider/ProviderHystrix8001Application.java
+++ provider-hystrix-8001/src/main/java/mjhct/springcloud/provider/ProviderHystrix8001Application.java
@@ -2,10 +2,12 @@ package mjhct.springcloud.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ProviderHystrix8001Application {
 
     public static void main(String[] args) {
--- provider-hystrix-8001/src/main/java/mjhct/springcloud/provider/service/ProviderServiceImpl.java
+++ provider-hystrix-8001/src/main/java/mjhct/springcloud/provider/service/ProviderServiceImpl.java
@@ -1,5 +1,7 @@
 package mjhct.springcloud.provider.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +13,9 @@ public class ProviderServiceImpl {
         return "秒回服务:" + Thread.currentThread().getName() + ",id=" + id;
     }
 
+    @HystrixCommand(fallbackMethod = "providerTimeOutHandler",commandProperties = {
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String providerTimeOut(Integer id){
         try {
             Thread.sleep(sleepTime);
@@ -18,6 +23,10 @@ public class ProviderServiceImpl {
             e.printStackTrace();
         }
         return "复杂服务:" + Thread.currentThread().getName() + ",id=" + id;
+    }
+
+    public String providerTimeOutHandler(Integer id){
+        return "兜底服务:" + Thread.currentThread().getName() + ",id=" + id;
     }
 
 }
