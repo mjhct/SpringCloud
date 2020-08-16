2ff6e4fa4c2a151a93519f8af9fe66fc4af0e89f
mjhct <mjhct0358@163.com>
2020-08-16 23:02:46 +0800

    Hystrix图形化监控。

--- provider-hystrix-8001/src/main/java/mjhct/springcloud/provider/ProviderHystrix8001Application.java
+++ provider-hystrix-8001/src/main/java/mjhct/springcloud/provider/ProviderHystrix8001Application.java
@@ -14,4 +14,23 @@ public class ProviderHystrix8001Application {
         SpringApplication.run(ProviderHystrix8001Application.class, args);
     }
 
+    /**
+     * 视频里的版本看不到监控数据是这样的问题和解决办法：
+     * 此配置是为了监控服务，与服务容错本身无关，SpringCloud升级后的问题。
+     * 不用管为什么了，这里知道用一下就可以了。
+     *
+     * 我用了最新的版本是另外一个问题，详情看hystrix-dashboard-9001依赖里面的说明吧。
+     * 我选择更换监控的版本，改源码麻烦而且也不实用……
+     * @return
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream"); // 这个路径就是要填的监控路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+     */
+
 }
