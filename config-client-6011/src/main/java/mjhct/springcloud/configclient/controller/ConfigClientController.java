11c90acc14e13c1b8db2ad45805b7d454e09ee5a
mjhct <mjhct0358@163.com>
2020-08-19 22:55:36 +0800

    SpringCloudConfig客户端手动动态刷新配置，避免服务重启。 刷新方法：POST请求客户端"/actuator/refresh"接口。

--- config-client-6011/src/main/java/mjhct/springcloud/configclient/controller/ConfigClientController.java
+++ config-client-6011/src/main/java/mjhct/springcloud/configclient/controller/ConfigClientController.java
@@ -1,19 +1,24 @@
 package mjhct.springcloud.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${config.value}")
     private String value;
 
+    @Value("${config.version}")
+    private String version;
+
     @GetMapping(value = "/value/get", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getValue(){
-        return value;
+        return value + version;
     }
 
 }
