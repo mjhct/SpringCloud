565a6fddb8da7e299aa95822a0f1883bab5115c4
mjhct <mjhct0358@163.com>
2020-09-21 22:09:44 +0800

    nacos配置中心演示，天生自动刷新。

--- nacos-consumer-83/src/main/java/mjhct/springcloud/consumer/controller/ConsumerController.java
+++ nacos-consumer-83/src/main/java/mjhct/springcloud/consumer/controller/ConsumerController.java
@@ -1,7 +1,9 @@
 package mjhct.springcloud.consumer.controller;
 
+import mjhct.springcloud.commons.entity.CommonCode;
 import mjhct.springcloud.commons.entity.CommonResult;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope
 public class ConsumerController {
 
     @Value("${server-url.provider-service}")
@@ -17,9 +20,17 @@ public class ConsumerController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Value("${config.info}")
+    private String configInfo;
+
     @GetMapping("/test")
     public CommonResult test() {
         return restTemplate.getForObject(providerServiceUrl + "/provider/test", CommonResult.class);
+    }
+
+    @GetMapping("/config/info")
+    public CommonResult<String> getConfigInfo() {
+        return new CommonResult<>(CommonCode.SUCCESS, configInfo);
     }
 
 }
