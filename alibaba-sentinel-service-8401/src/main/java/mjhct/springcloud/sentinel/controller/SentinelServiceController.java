9395a90ee46800533b489070cdb20bce3f8a479e
mjhct <mjhct0358@163.com>
2020-10-06 17:01:34 +0800

    sentinel热点规则。

--- alibaba-sentinel-service-8401/src/main/java/mjhct/springcloud/sentinel/controller/SentinelServiceController.java
+++ alibaba-sentinel-service-8401/src/main/java/mjhct/springcloud/sentinel/controller/SentinelServiceController.java
@@ -1,10 +1,13 @@
 package mjhct.springcloud.sentinel.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import mjhct.springcloud.commons.entity.CommonCode;
 import mjhct.springcloud.commons.entity.CommonResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,7 +24,20 @@ public class SentinelServiceController {
     @GetMapping(value = "/hello")
     public CommonResult hello() {
         logger.info("hello");
+//        int age = 10 / 0; 测试异常导服务降级
         return new CommonResult(CommonCode.SUCCESS);
+    }
+
+    @GetMapping(value = "/hk")
+    @SentinelResource(value = "hk", blockHandler = "handleHK")
+    public CommonResult hk(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        logger.info("test");
+        return new CommonResult(CommonCode.SUCCESS);
+    }
+
+    public CommonResult handleHK(String p1, String p2, BlockException blockException) {
+        return new CommonResult(CommonCode.FAIL, "热点key请求过多");
     }
 
 }
