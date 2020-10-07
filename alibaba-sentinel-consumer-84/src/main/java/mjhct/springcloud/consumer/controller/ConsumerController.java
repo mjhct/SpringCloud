fe757980cd0a02ba67c096c2fe554a324ff64e72
mjhct <mjhct0358@163.com>
2020-10-07 13:34:19 +0800

    sentinel注解@SentinelResource自定义降级熔断与限流配置。

--- alibaba-sentinel-consumer-84/src/main/java/mjhct/springcloud/consumer/controller/ConsumerController.java
+++ alibaba-sentinel-consumer-84/src/main/java/mjhct/springcloud/consumer/controller/ConsumerController.java
@@ -1,6 +1,8 @@
 package mjhct.springcloud.consumer.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import mjhct.springcloud.commons.entity.CommonCode;
 import mjhct.springcloud.commons.entity.CommonResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +24,10 @@ public class ConsumerController {
     private RestTemplate restTemplate;
 
     @GetMapping("/data/{id}")
+    // 文档https://github.com/alibaba/Sentinel/wiki/%E6%B3%A8%E8%A7%A3%E6%94%AF%E6%8C%81
+//    @SentinelResource(value = "consumerData", fallback = "getDataFallBack")
+//    @SentinelResource(value = "consumerData", blockHandler = "getDataBlockHandler")
+    @SentinelResource(value = "consumerData", fallback = "getDataFallBack", blockHandler = "getDataBlockHandler")
     public CommonResult getData(@PathVariable(value = "id") Integer id) {
         logger.info("消费者");
         if (id > 10) {
@@ -31,4 +37,15 @@ public class ConsumerController {
         }
         return restTemplate.getForObject(PROVIDER_URL + "/provider/data/" + id, CommonResult.class);
     }
+
+    public CommonResult getDataFallBack(Integer id, Throwable e) {
+        logger.info("微服务异常，降级熔断处理");
+        return new CommonResult(CommonCode.FAIL, "fallback:" + e.getMessage());
+    }
+
+    public CommonResult getDataBlockHandler(Integer id, BlockException blockException) {
+        logger.info("sentinel限流处理");
+        return new CommonResult(CommonCode.FAIL, "blockHandler:" + blockException.getMessage());
+    }
+
 }
