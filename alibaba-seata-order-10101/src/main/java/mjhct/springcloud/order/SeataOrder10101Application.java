ff11389f8c48f85d1545e7184d5e58ec4dadb4ab
mjhct <mjhct0358@163.com>
2020-10-18 17:32:28 +0800

    seata演示模块搭建，引入spring-data-jpa。

---
+++ alibaba-seata-order-10101/src/main/java/mjhct/springcloud/order/SeataOrder10101Application.java
@@ -0,0 +1,13 @@
+package mjhct.springcloud.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeataOrder10101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataOrder10101Application.class, args);
+    }
+
+}
